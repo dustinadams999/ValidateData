f2651320ffecc37b920d73b1f28062b9d193f2a4
Dustin Adams <dustinadams01@gmail.com>
2021-05-11 13:42:44 -0700

    Combining determing bad values and incorrect formatting all in one for loop.

--- validate_data.py
+++ validate_data.py
@@ -4,6 +4,9 @@
 # Can we safely assume this is all USA data? Seems to be the case.
 # Should we write the data that needs to be manually checked back to the csv file?
 # Or should we prompt the user?
+# What constitutes a "correct value"? Right now we're assuming a correct value is a value that is not bad and not incorrect formatting.
+
+# We're assuming a phone number is formatted as (123) 456-7890
 
 import itertools
 import json
@@ -33,6 +36,7 @@ def main(options):
 
     # data provided by https://simplemaps.com/data/us-zips
     zip_data = pd.read_csv('/Users/dustinadams/quicken_code_test/simplemaps_uszips_basicv1.77/uszips.csv')
+                
 
     # the above data set is not complete (has some missing zip codes) - also need to grab from this site https://www.zipcodestogo.com
     with open('additional_state_code_data.json', 'r') as zip_file:
@@ -57,49 +61,96 @@ def main(options):
         if pd.isnull(user_data['zip'][a]): zips_missing_indexes.append(a)
         if pd.isnull(user_data['phone1'][a]): phones_missing_indexes.append(a)
         if pd.isnull(user_data['email'][a]): emails_missing_indexes.append(a)
-    print('Number of missing social security numbers: {}'.format(user_data.isnull().sum()[0]))
+    
+    num_missing_soc_secs = len(soc_secs_missing_indexes) # TODO: change to length of the array
+    num_missing_states = len(states_missing_indexes)
+    num_missing_zips = len(zips_missing_indexes)
+    num_missing_phones = len(phones_missing_indexes)
+    num_missing_emails = len(emails_missing_indexes)
+
+    print('Number of missing social security numbers: {}'.format(num_missing_soc_secs))
     print('Indexes of missing social security numbers: {}\n'.format(soc_secs_missing_indexes))
-    print('Number of missing states: {}'.format(user_data.isnull().sum()[1]))
+    print('Number of missing states: {}'.format(num_missing_states))
     print('Indexes of missing states: {}\n'.format(states_missing_indexes))
-    print('Number of missing zip codes: {}'.format(user_data.isnull().sum()[2]))
+    print('Number of missing zip codes: {}'.format(num_missing_zips))
     print('Indexes of missing zip codes: {}\n'.format(zips_missing_indexes))
-    print('Number of missing phones: {}'.format(user_data.isnull().sum()[3]))
+    print('Number of missing phones: {}'.format(num_missing_phones))
     print('Indexes of missing phones: {}\n'.format(phones_missing_indexes))
-    print('Number of missing email addresses: {}'.format(user_data.isnull().sum()[4]))
+    print('Number of missing email addresses: {}'.format(num_missing_emails))
     print('Indexes of missing email addresses: {}\n'.format(emails_missing_indexes))
 
-    # TODO: identify and count correct values (of all columns)
-
-    # *************************
-    # CHECKING FOR BAD VALUES *
-    # *************************
-
-    # cycle through all records to find bad values and fix 
+    # cycle through all records to find bad, incorrect formatted (and fix), and correct values
     num_correct_soc_secs = 0
     num_correct_states = 0
     num_correct_zips = 0
     num_correct_phones = 0
     num_correct_emails = 0
     for a in range(user_data.shape[0]):
+        correct_soc_sec = True
+        correct_state = True
+        correct_zip = True
+        correct_phone = True
+        correct_email = True
         # social security: if the value is not XXX-XX-XXXX, and the value is not 9 ints, it's a bad value
         if not pd.isnull(user_data['social_security'][a]):
+            # bad value
             if not ((user_data['social_security'][a].replace('-','')).isnumeric() and len(user_data['social_security'][a].replace('-','')) == 9):
+                correct_soc_sec = False
                 print('INVALID SOCIAL SECURITY NUMBER. value: {} on index: {}'.format(user_data['social_security'][a], a))
+
+            # incorrect formatting
+            i = user_data['social_security'][a]
+            if ((i.isnumeric() and len(i) == 9)):
+                correct_soc_sec = False
+                user_data.at[a, 'social_security'] = i[0:3] + '-' + i[3:5] + '-' + i[5:]
 
         # state: If neither the state nor the 2 letter state code exists, then it's a bad value
         if not pd.isnull(user_data['state'][a]):
+            # bad value
             if (not user_data['state'][a] in state_codes.keys()) and (not user_data['state'][a] in state_codes.values()):
+                correct_state = False
                 print('INVALID STATE. value: {} on index: {}'.format(user_data['state'][a], a))
+
+            # incorrect formatting
+            i = user_data['state'][a]
+            if (i not in state_codes.values()):
+                correct_state = False
+                # check if i is a valid state
+                if i.lower().capitalize() in state_codes.keys(): # make sure to format the state correctly when searching
+                    user_data.at[a, 'state'] = state_codes[i.lower().capitalize()]
+
 
         # find invalid zip codes - if the zip code converted to an int does not exist, then it's a bad value
         if not pd.isnull(user_data['zip'][a]):
+            # bad value
             if not (user_data['zip'][a] in all_adtl_zips):
+                correct_zip = False
                 print('INVALID ZIP CODE. value: {} on index: {}'.format(user_data['zip'][a],a))
+
+            # incorrect formatting
+            # phone (123) 456-7890
+            i = user_data['phone1'][a]
+            if i[:2] == '+1': # it's already assumed we're in USA
+                correct_phone = False
+                i = i[2:].strip()
+            if (len(i) != 14) or (not (i[1:4]+i[6:9]+i[10:]).isnumeric()):
+                correct_phone = False
+            if len(i.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')) == 10:
+                i = i.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
+                i = '({}) {}-{}'.format(i[0:3], i[3:6], i[6:])
+                user_data.at[a, 'phone1'] = i
+
+
+        if not pd.isnull(user_data['email'][a]):
+            # bad value
+            if(not re.search(regex, user_data['email'][a])):
+                correct_email = False
+                print('INVALID EMAIL ADDRESS. email: {}, index: {}'.format(user_data['email'][a], a))
 
         # another layer of validation for zip codes will be to match it up with the state.
         # if the validation data does not match for the zip and state, then it's going to require manual inspection.
+        # TODO: we don't actually need the csv file - just use the json file
         if not (pd.isnull(user_data['state'][a]) or pd.isnull(user_data['zip'][a])):
-            #continue
             # for each record's state, make sure its zip code belongs to that state
             state_code = ''
             if user_data['state'][a] in state_codes.values():
@@ -109,75 +160,31 @@ def main(options):
 
             if state_code != '':
             # check that the value isn't in the adtl_zip_data 
-                if not (str(int(user_data['zip'][a])) in adtl_zip_data[state_code]):
+                if not (user_data['zip'][a] in adtl_zip_data[state_code]):
                     # get index of the zip code from the zip_data dataframe
                     zip_index = (zip_data['zip'][zip_data['zip'] == int(user_data['zip'][a])]).index[0]
                     if zip_data['state_id'][zip_index] != state_code:
                         print('ZIP CODE AND STATE DO NOT MATCH ERROR. state: {}, zip: {}, index: {}'.format(user_data['state'][a], user_data['zip'][a], a))
 
+        # not sure what would constitute an incorrectly formatted email...
 
-        if not pd.isnull(user_data['email'][a]):
-            if(not re.search(regex, user_data['email'][a])):
-                print('INVALID EMAIL ADDRESS. email: {}, index: {}'.format(user_data['email'][a], a))
+        if correct_soc_sec: num_correct_soc_secs += 1
+        if correct_state: num_correct_states += 1
+        if correct_zip: num_correct_zips +=1
+        if correct_phone: num_correct_phones += 1
+        if correct_email: num_correct_emails += 1
 
-    # TODO: Combine formatting into the above for loop
+    print('Correct social security numbers: {}'.format(num_correct_soc_secs - num_missing_soc_secs))
+    print('Correct states: {}'.format(num_correct_states - num_missing_states))
+    print('Correct zip codes: {}'.format(num_correct_zips - num_missing_zips))
+    print('Correct phone numbers: {}'.format(num_correct_phones - num_missing_phones))
+    print('Correct email addresses: {}'.format(num_correct_emails - num_missing_emails))
+
+
     # TODO: Create functions for each check
+    # TODO: add the path that was entered by the user for the below file
 
-    # ***********************************
-    # CHECKING FOR INCORRECT FORMATTING *
-    # ***********************************
-
-    print('FIXING FORMATTING')
-
-    # social security: if the value is not XXX-XX-XXXX (with dashes) and is 9 ints, add the dashes to the 9 ints
-    a = 0
-    for i in user_data['social_security']:
-        if pd.isnull(i):
-            a += 1
-            continue
-        if ((i.isnumeric() and len(i) == 9)):
-            user_data.at[a, 'social_security'] = i[0:3] + '-' + i[3:5] + '-' + i[5:]
-        a += 1    
-
-
-    # state: if it's not the two letter state code, assume it's the full state name and rename it to the 2 letter code
-    a = 0
-    for i in user_data['state']:
-        if pd.isnull(i):
-            a += 1
-            continue
-        if (i not in state_codes.values()):
-            # check if i is a valid state
-            if i.lower().capitalize() in state_codes.keys(): # make sure to format the state correctly when searching
-                user_data.at[a, 'state'] = state_codes[i.lower().capitalize()]
-            #user_data.at[a, 'social_security'] = i[0:3] + '-' + i[3:5] + '-' + i[5:]
-        a += 1 
-
-
-    # zip: if it's any other data type not an int, change it to an int
-
-    # phone: should be (XXX) XXX-XXXX, so if it's not then convert the 10 numbers to that.
-    # if there's a +1 at the beginning, get rid of it.
-    a = 0
-    for i in user_data['phone1']:
-        if pd.isnull(i):
-            a +=1
-            continue
-        if i[:2] == '+1': # it's assumed we're in USA
-            i = i[2:].strip()
-        if len(i.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')) == 10:
-            i = i.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
-            i = '({}) {}-{}'.format(i[0:3], i[3:6], i[6:])
-            user_data.at[a, 'phone1'] = i
-        a += 1
-
-
-    # TODO: Finally, produce the 'cleaned' dataset back
     user_data.to_csv('new_data_quality_case_study.csv', index=False)
-
-    # TODO: Make a list of all the assumptions that we're making here:
-    # ASSUMPTION: we're only working with USA data (this has implications for state, phone number, and zip)
-
 
 if __name__ == '__main__':
     from optparse import OptionParser
